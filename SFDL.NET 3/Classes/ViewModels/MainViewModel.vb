182233fbe5b0984d7e8715f744093b08bc3448d4
n0ix <n0ix@protonmail.com>
2018-02-17 17:42:03 +0100

    Switched to MetroProgressBar + Update to Version 3.0.3.2

--- SFDL.NET 3/Classes/ViewModels/MainViewModel.vb
+++ SFDL.NET 3/Classes/ViewModels/MainViewModel.vb
@@ -16,6 +16,7 @@ Imports OpenFileDialog = Microsoft.Win32.OpenFileDialog
 
 Public Class MainViewModel
     Inherits ViewModelBase
+    Implements IDisposable
 
     Private _settings As New Settings
 
@@ -101,7 +102,6 @@ Public Class MainViewModel
         Dim _log As Logger = LogManager.GetLogger("RunDownloadPathDialog")
         Dim m_customDialog = New CustomDialog() With {.Title = "Choose Download Folder"}
         Dim dataContext As SelectContainerDownloadPathPopUpVieModel
-        Dim _result As String
 
         dataContext = New SelectContainerDownloadPathPopUpVieModel(Sub(instance)
                                                                        _dialogCoordinator.HideMetroDialogAsync(Me, m_customDialog)
@@ -2247,6 +2247,38 @@ Decrypt:
             Return _containerinfo_totalsize
         End Get
     End Property
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' Dient zur Erkennung redundanter Aufrufe.
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: verwalteten Zustand (verwaltete Objekte) entsorgen.
+            End If
+
+            ' TODO: nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalize() weiter unten überschreiben.
+            ' TODO: große Felder auf Null setzen.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: Finalize() nur überschreiben, wenn Dispose(disposing As Boolean) weiter oben Code zur Bereinigung nicht verwalteter Ressourcen enthält.
+    'Protected Overrides Sub Finalize()
+    '    ' Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in Dispose(disposing As Boolean) weiter oben ein.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' Dieser Code wird von Visual Basic hinzugefügt, um das Dispose-Muster richtig zu implementieren.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Ändern Sie diesen Code nicht. Fügen Sie Bereinigungscode in Dispose(disposing As Boolean) weiter oben ein.
+        Dispose(True)
+        ' TODO: Auskommentierung der folgenden Zeile aufheben, wenn Finalize() oben überschrieben wird.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
 
 #End Region
 
