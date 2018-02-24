3e049842e5da4b027cba869dcca6ba3fe75ad46b
n0ix <n0ix@protonmail.com>
2018-02-24 20:21:35 +0100

    Fixed .installstate

--- SFDL.NET 3/Modules/FileRegisterHelper.vb
+++ SFDL.NET 3/Modules/FileRegisterHelper.vb
@@ -18,7 +18,7 @@ Module FileRegisterHelper
 
         Try
 
-            _sw = New StreamWriter(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, ".installstate"), False)
+            _sw = New StreamWriter(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "loader.installstate"), False)
             _sw.WriteLine(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory).ToString)
             _sw.Flush()
             _sw.Close()
@@ -33,13 +33,13 @@ Module FileRegisterHelper
 
         Dim _rt As Boolean = False
 
-        If IO.File.Exists(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, ".installstate")) Then
+        If IO.File.Exists(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "loader.installstate")) Then
 
             Dim _installstate_path As String
 
-            _installstate_path = My.Computer.FileSystem.ReadAllText(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, ".installstate")).Trim
+            _installstate_path = My.Computer.FileSystem.ReadAllText(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "loader.installstate")).Trim
 
-            If _installstate_path.Equals(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory).ToString) = False Then
+            If _installstate_path.ToLower.Equals(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory).ToString.ToLower.Trim) = False Then
                 _rt = False
             Else
                 _rt = True
